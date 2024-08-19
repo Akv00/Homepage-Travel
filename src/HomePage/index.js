eabcfe28e2a66d388953c2eee850553f59f768c8
Akv00 <kunal613.kv@gmail.com>
2024-08-19 19:50:01 +0530

    changed header styles

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -11,7 +11,7 @@ return (
         <p className="text-4xl pt-5">Book Your Happy Trip. Now!!</p>
         </div>
     </div>
-    <div className={`${styles.intro_block} px-56  text-lg font-medium items-center `} style={{
+    <div className={`${styles.intro_block} pl-56  text-lg font-medium items-center text-wrap `} style={{
     backgroundImage: `url('/beach.jpeg')`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
@@ -21,7 +21,7 @@ return (
         <div className={`${styles.intro_title} `}><div className="text-6xl font-serif font-semibold pr-20 ">Welcome to 1000 Mile Travel Vacations</div></div>
         <div className={`${styles.intro_body} flex font-serif flex-col text-2xl  `}>
           <h className="mb-5">For travelers who expect more, 1000 Mile Travel Vacations specialize in designing tailored vacation and travel experiences to make your travel dreams a reality.</h>
-          <p className="mb-5">From weekend getaways to extended, family-focused itineraries and luxury escapes, when you book with 1000 Mile Travel Vacations you’ll enjoy exemplary service and value for money, delivered by the best team in travel.</p>
+          <p className="mb-5 text-wrap">From weekend getaways to extended, family-focused itineraries and luxury escapes, when you book with 1000 Mile Travel Vacations you’ll enjoy exemplary service and value for money, delivered by the best team in travel.</p>
           <p>The 1000 Mile Travel Group is part of the global CTM Group of travel businesses. When you book your vacation through 1000 Mile Travel Vacations, you’ll enjoy the benefits of CTM’s global buying power and extensive partnerships which deliver exceptional value and access to unique travel ‘perks’ – making every holiday experience an unforgettable one.</p>
         </div>
     </div>
