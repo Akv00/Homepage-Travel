d34fb68b234d0a4f604d0aeb18041b4b7930ac42
Akv00 <kunal613.kv@gmail.com>
2024-08-19 19:58:31 +0530

    changed footer

--- src/ContactUs/index.js
+++ src/ContactUs/index.js
@@ -8,7 +8,7 @@ function ContactUs() {
     <div className={styles.contactUs}>
         <div className={styles.intro}>
         <div className={styles.company}>Let's Plan Your Travel Journey Now With <br></br><span style={{ color: '#CCB0FF' }}>Tavel Agency Name</span></div>
-        <div className={styles.social_media}><span>Follow us on</span> <div className={styles.icon}><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className={styles.icon}>
+        <div className={`${styles.social_media} text-5xl`}><span>Follow us on</span> <div className={styles.icon}><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className={styles.icon}>
             <FaInstagram  style={{ color: 'white'}}/>
           </a>
           <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className={styles.icon}>
@@ -17,8 +17,8 @@ function ContactUs() {
           </div>
         </div>
         </div>
-        <div className={styles.navigations}> 
-        <span style={{ color:'#CCB0FF' , fontWeight: '600' }}>Navigations</span>
+        <div className={`${styles.navigations} text-xl font-semibold`}> 
+        <span style={{ color:'#CCB0FF' , fontWeight: '700' }} className="text-3xl font-bold">Navigations</span>
         <Link to="home" smooth={true} duration={500} className={styles.scroll_link}>Home</Link>
         <Link to="ticket-booking" smooth={true} duration={500} className={styles.scroll_link}>Ticket Booking</Link>
         <Link to="visa-booking" smooth={true} duration={500} className={styles.scroll_link}>Visa Booking</Link>
@@ -27,20 +27,20 @@ function ContactUs() {
         <Link to="contact" smooth={true} duration={500} className={styles.scroll_link}>Contact Us</Link>
 
         </div>
-        <div className={styles.contact}>
+        <div className={`${styles.contact}`}>
             <div className={styles.header}>
-            <span style={{fontWeight: '600'}}>Contact Info</span>
-            <div className={styles.contactItem}>
+            <span style={{fontWeight: '600'}} className="text-3xl mb-5">Contact Info</span>
+            <div className={`${styles.contactItem} text-2xl`}>
             <FaPhone className={styles.icon} />
             <span>phone no.</span>
             </div>
-            <div className={styles.contactItem}>
+            <div className={`${styles.contactItem} text-2xl`}>
                 <FaEnvelope className={styles.icon} />
                 <span>emailhere@gmail.com</span>
             </div>
             </div>
             <div className={styles.footer}> <FaMapMarker className={styles.icon} />
-        <span>1234 Travel St, Wanderlust City, TX 12345</span></div>
+        <span className="text-xl">1234 Travel St, Wanderlust City, TX 12345</span></div>
         </div>
         
 
